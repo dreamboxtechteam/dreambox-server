a094de94ebcf88e8d018d018c1f71988e3338bb3
Chigozie Ohakwe <chigozieohakwej@gmail.com>
2026-03-28 15:32:47 +0100

    fixed port issues

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors(); // Allows your frontend to talk to this server
-  await app.listen(3000);
-  console.log(`🚀 Server running on: http://localhost:3000`);
+  
+  await app.listen(process.env.PORT || 3000);
+  console.log(`🚀 Server running on: http://localhost:${process.env.PORT || 3000}`);
 }
 bootstrap();
